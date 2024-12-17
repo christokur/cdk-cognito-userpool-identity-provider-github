a2609e6f06e61ae0d8910c6f48a8b44f17184a16
Christo De Lange <christo.delange@sands.com>
2024-12-17 12:53:12 -0500

    fix: github response

--- .projenrc.ts
+++ .projenrc.ts
@@ -50,7 +50,7 @@ class CognitoGithubProviderProject extends AwsCdkConstructLibrary {
       jsiiVersion: "5.6.0",
       deps: [],
       bundledDeps: [
-        "github-cognito-openid-wrapper@^1.3.19",
+        "github-cognito-openid-wrapper@^1.3.21",
         "source-map-support",
       ],
       devDeps: [
