149bb03f120d1bfcf4174a5cc4dc69b37384537e
Christo De Lange <christo.delange@sands.com>
2024-12-21 13:55:26 -0500

    feat: Newer API

--- src/user-pool-identity-provider-github.ts
+++ src/user-pool-identity-provider-github.ts
@@ -9,7 +9,7 @@ import { createApiGateway } from "./api-gateway";
 import { createLambdaFunction } from "./lambda";
 import { IUserPoolIdentityProviderGithubProps } from "./types";
 
-const VERSION: string = "2.0.19";
+const VERSION: string = "2.0.20";
 
 /**
  * GitHub OpenID Connect Wrapper for Cognito
