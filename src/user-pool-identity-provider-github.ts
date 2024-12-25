34525e3549162eee4ce83fe77c2868ea7d2ae84c
Christo De Lange <christo.delange@sands.com>
2024-12-25 00:38:36 -0500

    fix: 401 to 500

--- src/user-pool-identity-provider-github.ts
+++ src/user-pool-identity-provider-github.ts
@@ -9,7 +9,7 @@ import { createApiGateway } from "./api-gateway";
 import { createLambdaFunction } from "./lambda";
 import { IUserPoolIdentityProviderGithubProps } from "./types";
 
-const VERSION: string = "2.0.37";
+const VERSION: string = "2.0.38";
 
 /**
  * GitHub OpenID Connect Wrapper for Cognito
