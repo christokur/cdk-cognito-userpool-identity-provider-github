3ddd5e112c92ef93917ad6743e5b81444b83ef42
Christo De Lange <christo.delange@sands.com>
2024-12-26 09:59:32 -0500

    fix: sync logs

--- .projenrc.ts
+++ .projenrc.ts
@@ -50,7 +50,7 @@ class CognitoGithubProviderProject extends AwsCdkConstructLibrary {
       jsiiVersion: "5.6.0",
       deps: [],
       bundledDeps: [
-        "github-cognito-openid-wrapper@^1.7.6",
+        "github-cognito-openid-wrapper",
         "source-map-support",
       ],
       devDeps: [
