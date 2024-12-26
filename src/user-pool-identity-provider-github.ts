49b0b7fa65a3b950c5bfa3cb84b231a2d4dea30c
Christo De Lange <christo.delange@sands.com>
2024-12-26 16:16:30 -0500

    chore: bump

--- src/user-pool-identity-provider-github.ts
+++ src/user-pool-identity-provider-github.ts
@@ -9,7 +9,7 @@ import { createApiGateway } from "./api-gateway";
 import { createLambdaFunction } from "./lambda";
 import { IUserPoolIdentityProviderGithubProps } from "./types";
 
-const VERSION: string = "2.0.43";
+const VERSION: string = "2.0.44";
 
 /**
  * GitHub OpenID Connect Wrapper for Cognito
